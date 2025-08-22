16d98fc870b2698c606ee08d9603ea07ca23c832
Tom Albone <tbalbone@gmail.com>
2025-08-22 14:44:29 +0000

    Reformat into pep8 style

--- chapter5/main.py
+++ chapter5/main.py
@@ -3,7 +3,8 @@ from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlalchemy.orm import Session
 from datetime import date
 
-import crud, schemas
+import crud
+import schemas
 from database import SessionLocal
 
 api_description = """
@@ -27,12 +28,13 @@ they scored using SWC league scoring.
 Get information about all the SWC fantasy football leagues and the teams in them.
 """
 
-#FastAPI constructor with additional details added for OpenAPI Specification
+# FastAPI constructor with additional details added for OpenAPI Specification
 app = FastAPI(
-    description=api_description
+    description=api_description,
     title="Sports World Central (SWC) Fantasy Football API",
     version="0.1"
 )
+
 
 # Dependency
 def get_db():
@@ -42,60 +44,67 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/",tags=["analytics"])
+
+@app.get("/", tags=["analytics"])
 async def root():
     return {"message": "API health check successful"}
 
 
-@app.get("/v0/players/",
-         response_model=list[schemas.Player],
-         tags=["player"],
-         summary="Get a list of NFL players",
-         description="Retrieve a list of NFL players with optional filtering by various parameters.",
-         operation_id="v0_get_players"
-         )
+@app.get(
+    "/v0/players/",
+    response_model=list[schemas.Player],
+    tags=["player"],
+    summary="Get a list of NFL players",
+    description="Retrieve a list of NFL players with optional filtering by various parameters.",
+    operation_id="v0_get_players"
+)
 def read_players(
     skip: int = Query(0, description="The number of items to skip at the beginning of API call."),
     limit: int = Query(100, description="The number of records to return after the skipped records."),
     minimum_last_changed_date: date = Query(None, description="The minimum date of change that you want to return records. Exclude any records changed before this."),
     first_name: str = Query(None, description="The first name of the players to return"),
     last_name: str = Query(None, description="The last name of the players to return"),
-        db: Session = Depends(get_db)):
-
-    players = crud.get_players(db, 
-                skip=skip, 
-                limit=limit, 
-                min_last_changed_date=minimum_last_changed_date, 
-                first_name=first_name, 
-                last_name=last_name)
-    
+    db: Session = Depends(get_db)
+):
+    players = crud.get_players(
+        db,
+        skip=skip,
+        limit=limit,
+        min_last_changed_date=minimum_last_changed_date,
+        first_name=first_name,
+        last_name=last_name
+    )
     return players
 
-@app.get("/v0/players/{player_id}",
-         response_model=schemas.Player,
-         tags=["player"],
-         summary="Get one player using the Player ID, which is internal to SWC",
-         description="If you have an SWC Player ID of a player from another API call such as v0_get_players, you can call this API using the player ID",
-         response_description="One NFL player",
-         operation_id="v0_get_players_by_player_id"
-         )
-def read_player(player_id: int, 
-                db: Session = Depends(get_db)):
-    player = crud.get_player(db, 
-                             player_id=player_id)
+
+@app.get(
+    "/v0/players/{player_id}",
+    response_model=schemas.Player,
+    tags=["player"],
+    summary="Get one player using the Player ID, which is internal to SWC",
+    description="If you have an SWC Player ID of a player from another API call such as v0_get_players, you can call this API using the player ID",
+    response_description="One NFL player",
+    operation_id="v0_get_players_by_player_id"
+)
+def read_player(
+    player_id: int,
+    db: Session = Depends(get_db)
+):
+    player = crud.get_player(db, player_id=player_id)
     if player is None:
-        raise HTTPException(status_code=404, 
-                            detail="Player not found")
+        raise HTTPException(status_code=404, detail="Player not found")
     return player
 
-@app.get("/v0/performances/", 
-         response_model=list[schemas.Performance],
-         tags=["scoring"],
-         summary="Get a list of NFL player performances",
-         description="Retrieve a list of NFL player performances with optional filtering by various parameters.",
-         response_description="A list of NFL player performances",
-         operation_id="v0_get_performances"
-         )
+
+@app.get(
+    "/v0/performances/",
+    response_model=list[schemas.Performance],
+    tags=["scoring"],
+    summary="Get a list of NFL player performances",
+    description="Retrieve a list of NFL player performances with optional filtering by various parameters.",
+    response_description="A list of NFL player performances",
+    operation_id="v0_get_performances"
+)
 def read_performances(
     skip: int = Query(
         0, description="The number of items to skip at the beginning of API call."
@@ -114,28 +123,35 @@ def read_performances(
     )
     return performances
 
-@app.get("/v0/leagues/{league_id}",
-         response_model=schemas.League, 
-         tags=["membership"],
-         summary="Get one league using the League ID, which is internal to SWC",
-         description="If you have an SWC League ID of a league from another API call such as v0_get_leagues, you can call this API using the league ID",
-         response_description="One SWC league",
-         operation_id="v0_get_leagues_by_league_id")
-def read_league(league_id: int, db: Session = Depends(get_db)):
+
+@app.get(
+    "/v0/leagues/{league_id}",
+    response_model=schemas.League,
+    tags=["membership"],
+    summary="Get one league using the League ID, which is internal to SWC",
+    description="If you have an SWC League ID of a league from another API call such as v0_get_leagues, you can call this API using the league ID",
+    response_description="One SWC league",
+    operation_id="v0_get_leagues_by_league_id"
+)
+def read_league(
+    league_id: int,
+    db: Session = Depends(get_db)
+):
     league = crud.get_league(db, league_id=league_id)
     if league is None:
         raise HTTPException(status_code=404, detail="League not found")
     return league
 
 
-@app.get("/v0/leagues/",
-         response_model=list[schemas.League],
-         tags=["membership"],
-         summary="Get a list of SWC leagues",
-         description="Retrieve a list of SWC leagues with optional filtering by various parameters.",
-         response_description="A list of SWC leagues",
-         operation_id="v0_get_leagues"
-         )
+@app.get(
+    "/v0/leagues/",
+    response_model=list[schemas.League],
+    tags=["membership"],
+    summary="Get a list of SWC leagues",
+    description="Retrieve a list of SWC leagues with optional filtering by various parameters.",
+    response_description="A list of SWC leagues",
+    operation_id="v0_get_leagues"
+)
 def read_leagues(
     skip: int = Query(
         0, description="The number of items to skip at the beginning of API call."
@@ -161,14 +177,16 @@ def read_leagues(
     )
     return leagues
 
-@app.get("/v0/teams/",
-         response_model=list[schemas.Team],
-         tags=["membership"],
-         summary="Get a list of SWC teams",
-         description="Retrieve a list of SWC teams with optional filtering by various parameters.",
-         response_description="A list of SWC teams",
-         operation_id="v0_get_teams"
-         )
+
+@app.get(
+    "/v0/teams/",
+    response_model=list[schemas.Team],
+    tags=["membership"],
+    summary="Get a list of SWC teams",
+    description="Retrieve a list of SWC teams with optional filtering by various parameters.",
+    response_description="A list of SWC teams",
+    operation_id="v0_get_teams"
+)
 def read_teams(
     skip: int = Query(
         0, description="The number of items to skip at the beginning of API call."
@@ -200,16 +218,19 @@ def read_teams(
     return teams
 
 
-@app.get("/v0/counts/",
-         response_model=schemas.Counts,
-         tags=["analytics"],
-         summary="Get counts of various SWC entities",
-         description="Retrieve counts of leagues, teams, and players in the SWC.",
-         response_description="Counts of SWC entities",
-         operation_id="v0_get_counts")
+@app.get(
+    "/v0/counts/",
+    response_model=schemas.Counts,
+    tags=["analytics"],
+    summary="Get counts of various SWC entities",
+    description="Retrieve counts of leagues, teams, and players in the SWC.",
+    response_description="Counts of SWC entities",
+    operation_id="v0_get_counts"
+)
 def get_count(db: Session = Depends(get_db)):
     counts = schemas.Counts(
-        league_count = crud.get_league_count(db),
-        team_count = crud.get_team_count(db),
-        player_count = crud.get_player_count(db))
+        league_count=crud.get_league_count(db),
+        team_count=crud.get_team_count(db),
+        player_count=crud.get_player_count(db)
+    )
     return counts
