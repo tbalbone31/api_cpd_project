59133ae951024d251ae777e06da9300ec9c25a99
Tom Albone <tbalbone@gmail.com>
2025-09-02 14:08:55 +0000

    started to rebuild client around sdk

--- chapter7/sdk/src/swcpy/swc_client.py
+++ chapter7/sdk/src/swcpy/swc_client.py
@@ -1,6 +1,30 @@
 import httpx
+import swcpy.swc_config as config
+from .schemas import League, Team, Player, Performance
+from typing import List
+import backoff
+import logging
+logger = logging.getLogger(__name__)
 
 class SWCClient:
+    """Interacts with the SportsWorldCentral APOI.
+
+    This SDK Class simplifies the process of using the SWC Fantasy Football API.
+    It supports all the functions of the SWC API and returns validated data types.
+
+    Typical usage example:
+
+        client = SWCClient()
+        response = client.get_health_check()
+    
+    """
+
+    HEALTH_CHECK_ENDPOINT = "/"
+    LIST_LEAGUES_ENDPOINT = "/v0/leagues/"
+    LIST_PLAYERS_ENDPOINT = "/v0/players/"
+    LIST_PERFORMANCES_ENDPOINT = "/v0/performances/"
+    LIST_TEAMS_ENDPOINT = "/v0/teams/"
+    GET_COUNTS_ENDPOINT = "/v0/counts/"
 
     def __init__(self, swc_base_url: str):
         self.swc_base_url = swc_base_url
